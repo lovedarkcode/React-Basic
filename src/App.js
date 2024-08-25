44fee3888572470a139299823980f4c14a66d6e3
adityau <adityau.fns@gmail.com>
2024-08-25 11:36:31 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react';
+import Coffee from './coffee';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Coffee/>
+    <h1>Coffee and React</h1>
+    <p>Hello from React serries</p>
+    </>
+    
   );
 }
 
